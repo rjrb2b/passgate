ab679b20380a6ee7e0d64a75a0e55189bb21a765
rjr.b2b <>
2024-08-02 20:40:03 +0200

    no message

--- BE/java/passgate_java_v0.1/src/main/java/rjr/studio/passgate/conf/security/SecurityJwtToken.java
+++ BE/java/passgate_java_v0.1/src/main/java/rjr/studio/passgate/conf/security/SecurityJwtToken.java
@@ -12,23 +12,23 @@ import rjr.studio.passgate.dao.entity.UserEntity;
 @Component
 public class SecurityJwtToken {
 
-	private final Integer expirationTime = 5 * 60 * 1000; // m * s * 1000
-	private final String jwtSecret = "b5840bef8a0e3ed1a923f820a8611aa27ca196a8468dd9a9d499459148931d47";
+	private final Integer EXPIRATION_TIME = 5 * 60 * 1000; // m * s * 1000
+	private final String JWT_SECRET = "b5840bef8a0e3ed1a923f820a8611aa27ca196a8468dd9a9d499459148931d47";
 
 	public String jwtTokenGenerate(UserEntity userEntity) {
 
-		Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+		Key key = Keys.hmacShaKeyFor(JWT_SECRET.getBytes());
 
 		return Jwts.builder().setSubject(userEntity.getUsername()).claim("roles", userEntity.getRoles())
 				.setIssuedAt(new Date())
-				.setExpiration(new Date(new Date().getTime() + expirationTime))
+				.setExpiration(new Date(new Date().getTime() + EXPIRATION_TIME))
 				.signWith(key, SignatureAlgorithm.HS512).compact();
 	}
 
 	public boolean jwtTokenValidate(String token) {
 		try {
 			Jwts.parserBuilder().setSigningKey(
-					Keys.hmacShaKeyFor(jwtSecret.getBytes())).build().parseClaimsJws(token);
+					Keys.hmacShaKeyFor(JWT_SECRET.getBytes())).build().parseClaimsJws(token);
 			return true;
 		} catch (Exception e) {
 			System.out.println("Invalid JWT Token: " + e.getMessage());
